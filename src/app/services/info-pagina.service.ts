0aa9032b0882d49ab49e13dfb8a92002615a933f
cayetanogb22 <cayetanogb22@gmail.com>
2021-02-02 16:15:06 +0100

    Fin sección 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,14 +10,29 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargarda = false;
 
-  constructor(private http: HttpClient) {
-    // console.log('Servicio de infoPagina cargada');
+  equipo: any[] = [];
 
+  constructor(private http: HttpClient) {
+    
+    this.cargarInfo();
+    this.cargarEquipo();
+
+  }
+
+  private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
       .subscribe( (resp: InfoPagina) => {
         this.cargarda = true;
         this.info = resp;
+      });
+  }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-daa7a-default-rtdb.europe-west1.firebasedatabase.app/equipo.json')
+      .subscribe( (resp: any[]) => {
+        this.equipo = resp;
         console.log(resp);
       });
   }
+
 }
